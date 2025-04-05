3f225d3801469f393724a9a32abd354017ce3c9a
Matt Lyle <matthewlyle@gmail.com>
2025-04-04 23:09:55 -0400

    initial working version

--- combiner/combiner.py
+++ combiner/combiner.py
@@ -3,57 +3,91 @@
 import re
 import argparse
 import os
-import sys
 
 ####################################################################################################
 
 
 def read_file(filename):
 
+    new_dir = os.path.dirname(filename)
+    base_filename = os.path.basename(filename)
+
+    new_dir_aws = os.path.abspath(new_dir)
+
+    # print("=============================================")
+    # print("filename:      %s" % filename)
+    # print("current path:  %s" % os.getcwd())
+    # print("new_dir:       %s (%s)" % (new_dir, new_dir_aws))
+    # print("base_filename: %s" % base_filename)
+    # print()
+
+    # switch to the directory of the file
+    os.chdir(new_dir_aws)
+
     lines = []
     symbols = {}
 
-    with open(filename) as f:
+    with open(base_filename) as f:
         lines = f.readlines()
 
-        i = 0
-        while i < len(lines):
+    # first pass: remove comments and empty lines
+    i = 0
+    while i < len(lines):
 
-            # print("%3d [%s]" % (i, lines[i]))
+        # print("%3d [%s]" % (i, lines[i]))
 
-            # remove comments
-            comment_start = lines[i].find("#")
-            if comment_start != -1:
-                lines[i] = lines[i][:comment_start].strip()
-                lines[i] = lines[i].rstrip()
+        lines[i] = lines[i].rstrip()
 
-            # Skip empty lines
-            if len(lines[i]) == 0:
-                del lines[i]
-                continue
+        # remove comments
+        comment_start = lines[i].find("//")
+        if comment_start != -1:
+            lines[i] = lines[i][:comment_start].rstrip()
 
-            # include
-            match = re.match("^\\s*include\\s*<([-a-zA-Z0-9_./]+)>", lines[i])
-            if match:
-                include_filename = match.group(1)
-                print("including: %s" % include_filename)
-                include_lines, include_symbols = read_file(include_filename)
+        # remove block comments
+        # TODO: finish
 
-                del lines[i]
-                i = i - 1
+        # Skip empty lines
+        if len(lines[i]) == 0:
+            del lines[i]
+            continue
 
-                for include_i in range(len(include_lines)):
-                    lines.insert(include_i, include_lines[include_i])
-                    i += len(include_lines)
+        i = i + 1
 
-            # use
-            match = re.match("^\\s*use\\s*<([-a-zA-Z0-9_./]+)>", lines[i])
-            if match:
-                include_filename = match.group(1)
-                print("using: %s" % include_filename)
+    # second pass: process includes and use statements
+    i = 0
+    while i < len(lines):
 
-            # next line...
-            i = i + 1
+        # print("at %d = %s" % (i, lines[i]))
+
+        # include
+        match = re.match("^\\s*include\\s*<([-a-zA-Z0-9_./]+)>", lines[i])
+        if match:
+            include_filename = match.group(1)
+            print("including: %s" % include_filename)
+
+            # remove this include line
+            # lines[i] = "#>>> " + lines[i]
+            del lines[i]
+            i = i - 1
+
+            include_lines, include_symbols = read_file(include_filename)
+
+            # switch back to the original directory
+            os.chdir(new_dir_aws)
+
+            for include_i in range(len(include_lines)):
+                lines.insert(include_i, include_lines[include_i])
+            i += len(include_lines)
+
+        # use
+        match = re.match("^\\s*use\\s*<([-a-zA-Z0-9_./]+)>", lines[i])
+        if match:
+            use_filename = match.group(1)
+            print("using: %s" % use_filename)
+            raise NotImplementedError("use not implemented yet")
+
+        # next line...
+        i = i + 1
 
     return lines, symbols
 
@@ -62,32 +96,18 @@ def read_file(filename):
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Process OpenSCAD files.")
-    parser.add_argument("filename", help="The OpenSCAD file to process")
+    parser = argparse.ArgumentParser(description="Process OpenSCAD files with includes into a single combined file")
+    parser.add_argument("input", help="The OpenSCAD file to process")
+    parser.add_argument("output", help="The OpenSCAD file to write")
     args = parser.parse_args()
 
-    # Get absolute path of the input file
-    abs_path = os.path.abspath(args.filename)
-    # Get the directory containing the file
-    file_dir = os.path.dirname(abs_path)
-    # Get just the filename
-    base_filename = os.path.basename(abs_path)
+    with open(args.output, "w") as f:
+        lines, symbols = read_file(args.input)
+        for line in lines:
+            f.write(line)
+            f.write("\n")
 
-    # Change to the directory of the input file
-    orig_dir = os.getcwd()
-    if file_dir:
-        print(f"Changing directory to: {file_dir}")
-        os.chdir(file_dir)
-
-    try:
-        lines, symbols = read_file(base_filename)
-
-        for i in range(len(lines)):
-            print("%3d | %s" % (i, lines[i]))
-    finally:
-        # Change back to the original directory
-        if file_dir:
-            os.chdir(orig_dir)
+    return
 
 
 ####################################################################################################
